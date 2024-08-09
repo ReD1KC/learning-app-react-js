45d0be4535e055650ec949d349d80954e65d07c6
ReD1KC <tchirkok@gmail.com>
2024-08-09 13:19:23 +0700

    Little problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,47 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import "./App.css";
 import { PostList } from "./Components/Postlist";
+import { MyInput } from "./Components/UI/input/MyInput";
+import MyButton from "./Components/UI/button/Mybutton";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "JavaScript", body: "Description" },
-    { id: 2, title: "JavaScript", body: "Description" },
-    { id: 3, title: "JavaScript", body: "Description" },
-    { id: 4, title: "JavaScript", body: "Description" },
-    { id: 5, title: "JavaScript", body: "Description" },
-  ]);
+    const [posts, setPosts] = useState([
+        { id: 1, title: "JavaScript", body: "Description" },
+        { id: 2, title: "JavaScript", body: "Description" },
+        { id: 3, title: "JavaScript", body: "Description" },
+    ]);
 
-  const bodyInputRef = useRef();
-  const [title, setTitle] = useState("");
-  const addNewPost = (e) => {
-    e.preventDefault();
-  };
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const addNewPost = (e) => {
+        e.preventDefault();
+        const newPost = {
+            title,
+            body,
+        };
+        setPosts([...posts, newPost]);
+    };
 
-  return (
-    <>
-      <form action="">
-        <input
-          type="text"
-          placeholder="Название поста"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <input type="text" placeholder="Описание поста" ref={bodyInputRef} />
-        <button onClick={addNewPost}>Создание поста</button>
-      </form>
-      <PostList posts={posts} title={"Список постов 1"} />
-    </>
-  );
+    return (
+        <>
+            <form action="">
+                <MyInput
+                    type="text"
+                    placeholder="Название поста"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <MyInput
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                    type="text"
+                    placeholder="Описание поста"
+                />
+                <MyButton onClick={addNewPost}>Создание поста</MyButton>
+            </form>
+            <PostList posts={posts} title={"Список постов 1"} />
+        </>
+    );
 }
 
 export default App;
